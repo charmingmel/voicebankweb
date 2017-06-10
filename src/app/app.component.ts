23b0c904061f0a5ec07120f09ef4df097bb1f14d
Charming Mel <mel.michael007@gmail.com>
2017-06-10 07:27:18 +0100

    Add routing and header boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Welcome to voice over bank';
-}
+export class AppComponent { }
